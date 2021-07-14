45c23283e0e35a9005b77d6ba2d5dda8ad7c924e
Luis Prado <luis.prado@crifa.com>
2021-07-13 22:43:14 -0500

    Create 2021_07_14_033947_update_permissions.php
    fix migrations

---
+++ database/migrations/business/2021_07_14_033947_update_permissions.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdatePermissions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->integer('show_to_users')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
