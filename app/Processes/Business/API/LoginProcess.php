64cc702b35f7d58bf0cbd3affd0f407fc9320a4c
Luis Prado <luis.prado@crifa.com>
2021-07-14 21:55:32 -0500

    Update LoginProcess.php
    test

--- app/Processes/Business/API/LoginProcess.php
+++ app/Processes/Business/API/LoginProcess.php
@@ -66,18 +66,21 @@ class LoginProcess
         if (Hash::check($data['password'], $user->password)) {
             if ($user != null) {
                 if ($user->enabled == 0) {
+                    dd(1);
                     $response = [
                         'type' => 'error',
                         'text' => trans('auth.disabled_user')
                     ];
                 } else {
                     if ($user->hasRole('client')) {
+                        dd(2);
                         $response = [
                             'type' => 'success',
                             'text' => trans('auth.success'),
                             'userId' => $user->id
                         ];
                     } else {
+                        dd(3);
                         $response = [
                             'type' => 'error',
                             'text' => trans('auth.disabled_role')
@@ -85,12 +88,14 @@ class LoginProcess
                     }
                 }
             } else {
+                dd(4);
                 $response = [
                     'type' => 'error',
                     'text' => trans('auth.disabled_user')
                 ];
             }
         } else {
+            dd(5);
             $response = [
                 'type' => 'error',
                 'text' => trans('auth.user_password_error')
