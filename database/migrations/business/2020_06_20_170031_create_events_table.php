29e5ee0eb1b3dac2ff6b52fc9b8ed61441bac3fa
Luis Prado <luis.prado@crifa.com>
2022-02-06 10:05:44 -0500

    sw
    SW

--- database/migrations/business/2020_06_20_170031_create_events_table.php
+++ database/migrations/business/2020_06_20_170031_create_events_table.php
@@ -16,7 +16,7 @@ class CreateEventsTable extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->dateTime('date');
             $table->date('date_start');
             $table->date('date_end');
